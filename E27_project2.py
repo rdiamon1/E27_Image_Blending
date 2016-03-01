81ae3d7d75a5c4875b2bd0bf77897b0159c14bc4
rdiamon1 <rdiamon1@swarthmore.edu>
2016-02-29 20:10:19 -0500

    tried to reconstruct

--- E27_project2.py
+++ E27_project2.py
@@ -7,17 +7,20 @@ Project 2
 import cv2
 import numpy
 
+NUM_PYR = 15 #number of iterartions in the pyramid
 
 ########################
 
 def pyr_build(Gi):
 
   lp = []
+  h = Gi.shape[0]
+  w = Gi.shape[1]
 
   win = 'Image Window'
   cv2.namedWindow(win)
 
-  for i in range (4):
+  for i in range (NUM_PYR):
     #scale it down
     Gi1 = cv2.pyrDown(Gi)
     print 'Gi1 has shape', Gi1.shape
@@ -29,7 +32,6 @@ def pyr_build(Gi):
     Gi.astype(numpy.float32)
     uGi1.astype(numpy.float32)
     Li = Gi - uGi1
-
     lp.append(Li)
 
     cv2.imshow(win, 0.5 + 0.5 * (Li / numpy.abs(Li).max()))
@@ -40,10 +42,38 @@ def pyr_build(Gi):
   return lp
 
 #########################
+def pyr_reconstruct(lp):
 
-G0 = cv2.imread('Golden_Retriever.JPG')
+    win = 'Image Window'
+    cv2.namedWindow(win)
 
-h = G0.shape[0]
-w = G0.shape[1]
+    Rn = lp[-1]
+    h,w,d = Rn.shape
+    image = Rn
 
-pyr_build(G0)
+    Ri = Rn
+    for i in range(len(lp)-1,0,-1):
+        #uRi = cv2.pyrUp(Ri, dstsize=(w, h))
+
+        uRi = cv2.pyrUp(Ri)
+        uRi = cv2.resize(uRi,(w,h))
+
+        Ri1 = uRi + lp[i]
+        image += Ri1
+        Ri = Ri1
+
+    image.astype(numpy.uint8)
+    cv2.imshow(win, image)
+    while cv2.waitKey(15) < 0: pass
+
+#########################
+def main():
+    G0 = cv2.imread('Golden_Retriever.JPG')
+
+    h = G0.shape[0]
+    w = G0.shape[1]
+
+    lp = pyr_build(G0)
+    pyr_reconstruct(lp)
+
+main()
